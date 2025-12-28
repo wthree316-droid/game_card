536025b712f2366692227ae5ceebb067c0f7e903
wthree316-droid <wthree316@gmail.com>
2025-12-28 21:39:39 +0700

    Add Firebase Authentication to configuration

--- js/core/firebase-config.js
+++ js/core/firebase-config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js"; 
 
 export const firebaseConfig = {
     apiKey: "AIzaSyAd5lxzwrrJF3cgg3mvRe9ei0ZT0og2Y1Q",
@@ -11,5 +12,5 @@ export const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const db = getFirestore(app);
+export const auth = getAuth(app);
